a71fe0abdc9aa8fb101ffc4d525d645628f28df0
jayeebee <pnuthexsa311@gmail.com>
2021-04-06 17:00:34 -0400

    generate HTML file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const fs = require('fs');
+const generatePage = require('./src/page-template')
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  
-  
-  printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfiolo complete! Check out index.html to see the output!')
+})
